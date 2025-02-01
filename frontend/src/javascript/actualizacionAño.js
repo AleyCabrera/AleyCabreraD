aba8a9dc1adeb5e52b10eb1cdc2cc34bad4de68a
Aldescodev <aley_cabrera@soy.sena.edu.co>
2025-02-01 09:05:18 -0500

    Restarurar repositorio

--- frontend/src/javascript/actualizacionAño.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-        const yearSpan = document.querySelector('.derechos');
-        const currentYear = new Date().getFullYear(); // Obtiene el año actual
-        yearSpan.innerHTML = `&copy; ${currentYear} Aley Cabrera. Todos los derechos reservados.`;
-    });
